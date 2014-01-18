b9ac4e80c97fe92e3b584577bdf2d274e91d929b
Blurredbits <mmorris00@mac.com>
2014-01-17 20:08:53 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VinTracker::Application.config.secret_key_base = '54676c748bdbafb07c065c24e724753f59345344bbebc887247d1c75eab4ec83bec7b9d6f9b74c938518f2532474a1976e5784a1fc9f65fd0ad7ea34fb0d8b9f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VinTracker::Application.config.session_store :cookie_store, key: '_vin_tracker_session'
